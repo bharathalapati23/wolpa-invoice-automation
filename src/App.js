a8cb599ea98407b2b2cba7a583a9a61e7a103dea
dartvader-20 <chandanmohan50@gmail.com>
2021-09-16 21:28:22 +0530

    New3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './App.css';
+import HomePage from "./TenantInvoice/HomePage";
 import TenantInvoice from './TenantInvoice/TenantInvoice'
-
+import OwnerInvoice from "./TenantInvoice/OwnerInvoive";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
-console.log(1)
   return (
-    <TenantInvoice />
+    <Router>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/tenantinvoice' component={TenantInvoice}/>
+          <Route exact path='/ownerinvoice' component={OwnerInvoice}/>
+        </Switch>
+      </Router>
   )
 }
 
